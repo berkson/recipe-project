58590ff5339b2f82035f9789417c3bb6a5e99295
berkson <berksonx@yahoo.com.br>
2021-06-08 23:17:16 -0300

    correção no controller

--- src/main/java/com/ximenes/recipeproject/controllers/IndexController.java
+++ src/main/java/com/ximenes/recipeproject/controllers/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
-    public String getindexPage(){
+    public String getIndexPage(){
         return "index";
     }
 }
