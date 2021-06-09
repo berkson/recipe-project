bc3411c28a4700687f3f53b12ed44d1e60ecbf0a
berkson <berksonx@yahoo.com.br>
2021-06-08 23:16:10 -0300

    Criados a index e o controller da index

---
+++ src/main/java/com/ximenes/recipeproject/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.ximenes.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Berkson Ximenes
+ * Date: 08/06/2021
+ * Time: 22:47
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getindexPage(){
+        return "index";
+    }
+}
