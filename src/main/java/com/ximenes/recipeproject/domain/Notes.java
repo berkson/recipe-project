39c197a4c7faab2f593190cf49acdefffe9f51f7
Berkson <berksonx@yahoo.com.br>
2021-06-13 22:13:43 -0300

    criação das entidades receita e notas de receita.

---
+++ src/main/java/com/ximenes/recipeproject/domain/Notes.java
@@ -0,0 +1,46 @@
+package com.ximenes.recipeproject.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by berkson
+ * Date: 13/06/2021
+ * Time: 21:40
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob // Creates a large object string on database.
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
