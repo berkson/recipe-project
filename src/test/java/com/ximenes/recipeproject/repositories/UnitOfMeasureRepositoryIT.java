ac851c6b3c9d6affcd87bdcedd83533a26809149
Berkson <berksonx@yahoo.com.br>
2021-06-22 08:20:40 -0300

    Realizado teste de integração com UnitOMeasureRepository

---
+++ src/test/java/com/ximenes/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,43 @@
+package com.ximenes.recipeproject.repositories;
+
+import com.ximenes.recipeproject.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by berkson
+ * Date: 22/06/2021
+ * Time: 07:48
+ */
+// could be used @SpringBootTest but @DataJpaTest is fast. bring a embedded database and configure jpa to us.
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
