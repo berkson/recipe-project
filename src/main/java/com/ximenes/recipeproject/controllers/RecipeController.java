e89aae7b1981e0b81d1735777476a98989b13845
Berkson <berksonx@yahoo.com.br>
2021-06-28 23:16:42 -0300

    Criação do form de cadastramento de receitas. criação do método que mostra o form e do método que salva o a receita. #Contém erros no form correção nas próximas aulas

--- src/main/java/com/ximenes/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/ximenes/recipeproject/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.ximenes.recipeproject.controllers;
 
+import com.ximenes.recipeproject.commands.RecipeCommand;
 import com.ximenes.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +28,18 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe/")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
